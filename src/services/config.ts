57b7190ca9d9f8f2419645d995263791e79c54ad
jevo1900 <jevo1900@gmail.com>
2023-07-04 15:21:28 +0200

    update config

--- src/services/config.ts
+++ src/services/config.ts
@@ -2,9 +2,9 @@ export class CONFIG {
     // static AUTH_URL = "http://localhost:8000/api/auth/";
     // static AUTH_URL_NEW = "http://localhost:8001/";
     // static API_URL = "http://localhost:8001/";
-    static AUTH_URL = process.env.AUTH_URL;
-    static API_URL = process.env.API_URL;
-    // static AUTH_URL = "https://anovelsite.com/api/auth/";
-    // static AUTH_URL_NEW = "https://anovelsite.com/api/auth/";
-    // static API_URL = "https://anovelsite.com/api/";
+    // static AUTH_URL = process.env.AUTH_URL;
+    // static API_URL = process.env.API_URL;
+    static AUTH_URL = "https://api.anovelsite.com/api/auth/";
+    static AUTH_URL_NEW = "https://api.anovelsite.com/api/auth/";
+    static API_URL = "https://api.anovelsite.com/api/";
 }
