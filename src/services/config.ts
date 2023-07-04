fa832c3f7f23a4b12875221ffadc2f8246964470
jevo1900 <jevo1900@gmail.com>
2023-07-04 15:22:45 +0200

    auth api url update

--- src/services/config.ts
+++ src/services/config.ts
@@ -4,7 +4,7 @@ export class CONFIG {
     // static API_URL = "http://localhost:8001/";
     // static AUTH_URL = process.env.AUTH_URL;
     // static API_URL = process.env.API_URL;
-    static AUTH_URL = "https://api.anovelsite.com/api/auth/";
-    static AUTH_URL_NEW = "https://api.anovelsite.com/api/auth/";
+    static AUTH_URL = "https://api.anovelsite.com/auth/";
+    static AUTH_URL_NEW = "https://api.anovelsite.com/auth/";
     static API_URL = "https://api.anovelsite.com/api/";
 }
